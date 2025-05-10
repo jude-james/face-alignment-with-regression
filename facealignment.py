a7fb5db742d235f34b2ba57587b344ba89470f79
Jude James <judejames04@hotmail.co.uk>
2025-05-10 13:56:33 +0100

    Add predict function for modular prediction on any set of images

--- facealignment.py
+++ facealignment.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import cv2
 from sklearn import linear_model
 from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
 
 # Load the train data using np.load
 train_data = np.load('face_alignment_training_images.npz', allow_pickle=True)
@@ -17,13 +18,13 @@ test_data = np.load('face_alignment_test_images.npz', allow_pickle=True)
 # Extract the testing images
 test_images = test_data['images']
 
-# Preprocess an images array
+# Preprocess an array of images
 def preprocess(images, scale):
     preprocess_images = []
     for img in images:
         # Resize the image to the new scale
         resized_img = cv2.resize(img, [int(img.shape[0] * scale), int(img.shape[1] * scale)])
-        # Convert to greyscale for SIFT
+        # Convert to greyscale
         greyscaled_img = cv2.cvtColor(resized_img, cv2.COLOR_RGB2GRAY)
         preprocess_images.append(greyscaled_img)
     return np.array(preprocess_images)
@@ -49,8 +50,6 @@ average_points = np.mean(train_points_resized, axis=0)
 print("average points:\n", average_points)
 
 # Get a train/test split
-from sklearn.model_selection import train_test_split
-
 train_images_split, test_images_split, train_points_split, test_points_split = train_test_split(
     train_images_preprocessed, train_points_resized, test_size=0.2, random_state=69
 )
@@ -65,25 +64,23 @@ def compute_descriptors(image, points):
     keypoints, descriptors = sift.compute(image, keypoints)  
     return descriptors
 
-def cascaded_regression(number_of_regressors, damping_factors, train_images, train_points):
+def cascaded_regression(num_regressors, damping_factors, train_images, train_points):
     num_images = len(train_images)
-
     predicted_train_points = [None] * num_images
     regressors = []
 
-    for i in range(number_of_regressors):
+    for i in range(num_regressors):
         x_train, y_train = [], []
 
         for j in range(num_images):
             img = train_images[j]
-            ground_truth_train_points = train_points[j]
 
             if predicted_train_points[j] is None:
                 predicted_train_points[j] = average_points.copy()
             
             # Compute SIFT descriptors based on the current prediction
             descriptors = compute_descriptors(img, predicted_train_points[j])
-            delta = (ground_truth_train_points - predicted_train_points[j]).flatten()
+            delta = (train_points[j] - predicted_train_points[j]).flatten()
 
             x_train.append(descriptors.flatten())
             y_train.append(damping_factors[i] * delta)
@@ -98,8 +95,7 @@ def cascaded_regression(number_of_regressors, damping_factors, train_images, tra
         # Now update predictions for all images
         for k in range(num_images):
             img = train_images[k]
-            predicted_points = predicted_train_points[k]
-            descriptors = compute_descriptors(img, predicted_points)
+            descriptors = compute_descriptors(img, predicted_train_points[k])
 
             x_feat = descriptors.flatten().reshape(1, -1)
             delta = model.predict(x_feat).reshape(-1, 2)
@@ -108,26 +104,32 @@ def cascaded_regression(number_of_regressors, damping_factors, train_images, tra
 
     return regressors
 
-num_regressors = 8
+def regression_predict(images, regressors):
+    predictions = []
+    for img in images:
+        predicted_points = average_points.copy()
+        for i in range(len(regressors)):
+            regressor = regressors[i]
+            descriptors = compute_descriptors(img, predicted_points)
+
+            x_feat = descriptors.flatten().reshape(1, -1)
+            delta = regressor.predict(x_feat).reshape(-1, 2)
+
+            predicted_points = predicted_points + damping_factors[i] * delta
+
+        predictions.append(predicted_points)
+    
+    return(np.array(predictions))
+
+num_pregressors = 8
 damping_factors = [1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.2]
-regressors = cascaded_regression(num_regressors, damping_factors, train_images_preprocessed, train_points_resized)
-
-test_predictions = []
-for img in test_images_preprocessed:
-    predicted_points = average_points.copy()
-    for i in range(len(regressors)):
-        regressor = regressors[i]
-        descriptors = compute_descriptors(img, predicted_points)
-
-        x_feat = descriptors.flatten().reshape(1, -1)
-        delta = regressor.predict(x_feat).reshape(-1, 2)
-
-        predicted_points = predicted_points + damping_factors[i] * delta
-
-    test_predictions.append(predicted_points)
-
-test_predictions = np.array(test_predictions)
+# run cascaded regression with the train images and train points
+regressors = cascaded_regression(num_pregressors, damping_factors, train_images_preprocessed, train_points_resized)
+# then get predictions on the test images
+test_predictions = regression_predict(test_images_preprocessed, regressors)
 print("final predictions shape:", test_predictions.shape)
+
+# TODO now run on test/train split and evaluate
 
 def visualise_pts_2(img, pts, pts2):
     plt.imshow(img, cmap='gray')
@@ -135,25 +137,9 @@ def visualise_pts_2(img, pts, pts2):
     plt.plot(pts2[:, 0], pts2[:, 1], '+g')
     plt.show()
 
-for i in range(200, 300):
-    visualise_pts_2(test_images_preprocessed[i], test_predictions[i], average_points)
-
 def visualise_pts(img, pts):
     plt.imshow(img, cmap='gray')
     plt.plot(pts[:, 0], pts[:, 1], '+r')
-    plt.show()
-
-'''
-for i in range(10):
-    idx = np.random.randint(0, train_images_preprocessed.shape[0])
-    visualise_pts_2(train_images_preprocessed[idx, ...], train_points_resized[idx, ...], predefined_points)
-'''
-    
-# This function will draw the keypoints on the images, showing their orientation
-def draw_im_kps(img, kps):  
-    image_with_kp = cv2.drawKeypoints(img, kps, None, flags=4)
-    plt.imshow(image_with_kp, cmap='gray')
-    plt.title("SIFT keypoints")
     plt.show()
 
 # TODO use this for evaluating data
@@ -182,3 +168,8 @@ def save_as_csv(points, location = '.'):
 # Resize the prediction points back to the original size
 test_predictions_resized = resize_points(test_predictions, 1 / resize_scale)
 save_as_csv(test_predictions_resized)
+
+# now finally visualise the original size predictions with the original test images
+for i in range(10):
+    idx = np.random.randint(0, test_images.shape[0])
+    visualise_pts(test_images[i], test_predictions_resized[i])
